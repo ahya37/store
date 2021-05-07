4c8472e168b15465444c7197e39095b90021b8c2
ahya37 <ahmadyaniid37@gmail.com>
2021-05-07 16:35:57 +0700

    db

--- database/migrations/2021_05_07_093303_change_nullable_at_users_table.php
+++ database/migrations/2021_05_07_093303_change_nullable_at_users_table.php
@@ -34,7 +34,8 @@ class ChangeNullableAtUsersTable extends Migration
      */
     public function down()
     {
-        $table->longText('address_one')->nullable(false)->change();
+        Schema::table('users', function (Blueprint $table) {
+            $table->longText('address_one')->nullable(false)->change();
             $table->longText('address_two')->nullable(false)->change();
             $table->integer('provinces_id')->nullable(false)->change();
             $table->integer('regencies_id')->nullable(false)->change();
@@ -44,5 +45,6 @@ class ChangeNullableAtUsersTable extends Migration
             $table->string('store_name')->nullable(false)->change();
             $table->integer('categories_id')->nullable(false)->change();
             $table->integer('store_status')->nullable(false)->change();
+        });
     }
 }
