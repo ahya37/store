2bb7843525d76f12188d814aef7e5fdb2c9ffd35
ahya37 <ahmadyaniid37@gmail.com>
2021-05-10 09:31:26 +0700

    merubah bahasa

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
       <div class="container">
         <div class="row">
           <div class="col-12 text-center">
-            <p class="pt-4 pb-2">2020 Copyright Store. All Right Reserved</p>
+            <p class="pt-4 pb-2">2021 Copyright Percikanshop. All Right Reserved</p>
           </div>
         </div>
       </div>
