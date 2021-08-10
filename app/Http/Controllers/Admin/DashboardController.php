edfd20ccfa6b4efc662bf0a7308c8131fd94dd77
ahya37 <ahmadyaniid37@gmail.com>
2021-08-10 09:13:22 +0700

    penambahan menu order

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,11 +6,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
 use App\Transaction;
+use AUth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        if (Auth::user()->access == 'EMPLOY') {
+            return redirect()->route('order.index');
+        }
+
         $customer = User::count();
         $revenue  = Transaction::sum('total_price');
         $transaction  = Transaction::count();
